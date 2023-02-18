e101d2653613c5841c9d975dc5d8b9fef0866a44
susaasus1 <313089@niuitmo.ru>
2023-02-18 13:38:12 +0300

    update

--- src/main/java/com/example/blps_lab1/model/RecipeIngredients.java
+++ src/main/java/com/example/blps_lab1/model/RecipeIngredients.java
@@ -1,63 +1,19 @@
 package com.example.blps_lab1.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "recipe_ingredients")
+@Data
+@NoArgsConstructor
 public class RecipeIngredients {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "recipe_id")
-    private Recipe recipe;
-
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "ingredient_id")
-    private Ingredients ingredient;
+    @EmbeddedId
+    private RecipeIngredientsPK id;
 
     private Double gram;
 
-    public RecipeIngredients( Recipe recipe, Ingredients ingredient, Double gram) {
-        this.recipe = recipe;
-        this.ingredient = ingredient;
-        this.gram = gram;
-    }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Recipe getRecipe() {
-        return recipe;
-    }
-
-    public void setRecipe(Recipe recipe) {
-        this.recipe = recipe;
-    }
-
-    public Ingredients getIngredient() {
-        return ingredient;
-    }
-
-    public void setIngredient(Ingredients ingredient) {
-        this.ingredient = ingredient;
-    }
-
-    public Double getGram() {
-        return gram;
-    }
-
-    public void setGram(Double gram) {
-        this.gram = gram;
-    }
-
-    public RecipeIngredients() {
-    }
 }
